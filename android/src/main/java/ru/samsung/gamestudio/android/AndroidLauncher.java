db27aafe75d60b402213afa94af8489a2613b336
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-01-13 15:25:59 +0300

    first commit

---
+++ android/src/main/java/ru/samsung/gamestudio/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.samsung.gamestudio.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.samsung.gamestudio.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
