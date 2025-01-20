7f0c872da48adfa00e820556cbefd4880a0aef3d
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-01-20 17:25:09 +0300

    screen saver impl

---
+++ core/src/main/java/ru/samsung/gamestudio/GamesSettings.java
@@ -0,0 +1,8 @@
+package ru.samsung.gamestudio;
+
+public class GamesSettings {
+
+    public static final int SCREEN_WIGHT = 600;
+    public static final int SCREEN_HEIGHT = 600;
+
+}
--- core/src/main/java/ru/samsung/gamestudio/Main.java
+++ core/src/main/java/ru/samsung/gamestudio/Main.java
@@ -1,28 +1,51 @@
 package ru.samsung.gamestudio;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
+
     private SpriteBatch batch;
     private Texture image;
+
+    Circle circle;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("texture/circle.png");
+        // blueCircle = new Texture("texture/circle2.png");
+
+        String[] paths = {
+            "texture/circle2.png",
+            "texture/circle.png"
+        };
+
+        circle = new Circle(
+            20, 300,
+            150, 5,
+            paths
+        );
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        circle.move();
+
+        ScreenUtils.clear(1f, 1f, 1f, 1f);
+
         batch.begin();
-        batch.draw(image, 140, 210);
+
+        // batch.draw(image, 0, 0, circleSize, circleSize);
+        // batch.draw(image, 0, GamesSettings.SCREEN_HEIGHT - circleSize, circleSize, circleSize);
+        // batch.draw(image, GamesSettings.SCREEN_WIGHT - circleSize, 0, circleSize, circleSize);
+        // batch.draw(image, GamesSettings.SCREEN_WIGHT - circleSize, GamesSettings.SCREEN_HEIGHT - circleSize, circleSize, circleSize);
+
+        circle.draw(batch);
+
         batch.end();
     }
 
