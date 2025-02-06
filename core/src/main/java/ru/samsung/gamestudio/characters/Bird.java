3151cd624ca077c18e6498f20292a575171ac720
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-02-06 17:10:43 +0300

    add movement for bird

---
+++ core/src/main/java/ru/samsung/gamestudio/characters/Bird.java
@@ -0,0 +1,69 @@
+package ru.samsung.gamestudio.characters;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Bird {
+
+    int x;
+    int y;
+    int width;
+    int height;
+
+    int speed;
+
+    final int maxHeightOfFly = 100;
+    int heightOfFly;
+    boolean isFlying;
+
+    Texture[] textures;
+    int textureCounter;
+
+    public Bird(int x, int y, int width, int height, int speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+
+        textureCounter = 0;
+        textures = new Texture[] {
+            new Texture("birdTiles/bird0.png"),
+            new Texture("birdTiles/bird1.png"),
+            new Texture("birdTiles/bird2.png"),
+            new Texture("birdTiles/bird1.png"),
+        };
+    }
+
+    public void onClick() {
+        isFlying = true;
+        heightOfFly = y + maxHeightOfFly;
+    }
+
+    public void fly() {
+
+        if (isFlying) {
+            y += speed;
+        } else {
+            y -= speed;
+        }
+
+        if (y > heightOfFly) {
+            isFlying = false;
+        }
+
+    }
+
+    public void draw(SpriteBatch batch) {
+        int frameFactor = 30;
+        textureCounter = (textureCounter + 1) % (textures.length * frameFactor);
+        batch.draw(textures[textureCounter / frameFactor], x, y, width, height);
+    }
+
+    public void dispose() {
+        for (Texture texture : textures) {
+            texture.dispose();
+        }
+    }
+
+}
--- core/src/main/java/ru/samsung/gamestudio/screens/GameScreen.java
+++ core/src/main/java/ru/samsung/gamestudio/screens/GameScreen.java
@@ -1,19 +1,26 @@
 package ru.samsung.gamestudio.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import ru.samsung.gamestudio.GamesSettings;
 import ru.samsung.gamestudio.Main;
+import ru.samsung.gamestudio.characters.Bird;
 
 public class GameScreen implements Screen {
 
-    Texture circle;
     Main main;
+    Bird bird;
 
     public GameScreen(Main main) {
         this.main = main;
-        circle = new Texture("texture/circle.png");
+
+        bird = new Bird(
+            100, GamesSettings.SCREEN_HEIGHT / 2 - 100 / 2,
+            100, 100,
+            5
+        );
     }
 
     @Override
@@ -24,14 +31,18 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(1f, 1f, 1f, 1f);
+        if (Gdx.input.justTouched()) {
+            bird.onClick();
+        }
 
+        bird.fly();
+
+        ScreenUtils.clear(1f, 1f, 1f, 1f);
         main.camera.update();
         main.batch.setProjectionMatrix(main.camera.combined);
 
         main.batch.begin();
-        main.batch.draw(circle, 0, 0, 100, 100);
-        main.batch.draw(circle, GamesSettings.SCREEN_WIGHT - 100, GamesSettings.SCREEN_HEIGHT - 100, 100, 100);
+        bird.draw(main.batch);
         main.batch.end();
 
     }
@@ -58,6 +69,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        bird.dispose();
     }
 }
